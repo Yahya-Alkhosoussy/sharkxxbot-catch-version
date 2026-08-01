f9c6cd8888e8edffadfb9b26846a09a494354a8b
Yahya Alkhosoussy <theapexnoob8@gmail.com>
2026-08-01 22:05:15 +0300

    Updated utils to include size and boost in the fish class, and added a helper function that gets the value of a fish by its size, and a property of the fish that will return its coin value based on rarity and size.
    Made a nets data class that will give the catch odds of a net of our choice and renamed the old nets class to NetsEnum.
    
    Made a boost dataclass to help manage stuff in one place, and added a size enum class.

--- fishing/utils.py
+++ fishing/utils.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from enum import StrEnum
 
 
-class Nets(StrEnum):
+class NetsEnum(StrEnum):
     ROPE = "rope net"
     LEATHER = "leather net"
     GOLD = "gold net"
@@ -15,7 +15,82 @@ class Rarity(StrEnum):
     SHINY = "shiny"
     LEGENDARY = "legendary"
 
+class Size(StrEnum):
+    LARGE = "large"
+    MEDIUM = "medium"
+    SMALL = "small"
+
+@dataclass(frozen=True)
+class Boost:
+    boost: bool
+    amount: int
+
+    def __post_init__(self) -> None:
+        if self.amount <= 0:
+            raise ValueError("Boost amount cannot be less than or equal to 0")
+
+
+@dataclass(frozen=True)
+class Nets:
+    net: NetsEnum
+
+    @property
+    def get_catch_odds(self) -> int:
+        match self.net:
+            case NetsEnum.DOOM:
+                return 100
+            case NetsEnum.TITANIUM:
+                return 100
+            case NetsEnum.GOLD:
+                return 85
+            case NetsEnum.LEATHER:
+                return 75
+            case NetsEnum.ROPE:
+                return 60
+            case _:
+                raise ValueError("Net is not in the known nets list")
+
 @dataclass(frozen=True)
 class Fish:
     rarity: Rarity
     net_used: Nets
+    size: Size
+    boost: Boost
+
+    def __get_fish_value_size(self, size: Size, normal_amount: int) -> int:
+        match size:
+            case Size.LARGE:
+                return normal_amount + 4
+            case Size.MEDIUM:
+                return normal_amount + 2
+            case Size.SMALL:
+                return normal_amount
+            case _:
+                raise ValueError("Size isn't one of the known options")
+
+
+    @property
+    def get_coin_value(self) -> int:
+        if self.boost:
+            match self.rarity:
+                case Rarity.TRASH:
+                    return 1 * self.boost.amount
+                case Rarity.COMMON:
+                    return self.__get_fish_value_size(self.size, 2) * self.boost.amount
+                case Rarity.SHINY:
+                    return self.__get_fish_value_size(self.size, 5) * self.boost.amount
+                case Rarity.LEGENDARY:
+                    return self.__get_fish_value_size(self.size, 10) * self.boost.amount
+                case _:
+                    raise ValueError("Rarity is not in the known list of rarities.")
+        match self.rarity:
+            case Rarity.TRASH:
+                return 1
+            case Rarity.COMMON:
+                return self.__get_fish_value_size(self.size, 2)
+            case Rarity.SHINY:
+                return self.__get_fish_value_size(self.size, 5)
+            case Rarity.LEGENDARY:
+                return self.__get_fish_value_size(self.size, 10)
+            case _:
+                raise ValueError("Rarity is not in the known list of rarities.")
