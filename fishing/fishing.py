94d1fdd242f0a1ff0d4ba2860729ef0494c8cd5f
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-09-20 11:21:01 +0100

    Finalized the fishing command and finished the helper function

--- fishing/fishing.py
+++ fishing/fishing.py
@@ -1,9 +1,12 @@
-from fishing.utils import Fish, Nets, Rarity, Size, Boost, Shark, SharkRarity # noqa
-from twitchAPI.chat import Chat, ChatCommand
-from fishing.fishing_db_interaction import is_net_available, get_shark_names, add_shark, add_fish # noqa
-from random import randint
 from datetime import datetime
+from random import randint
 from zoneinfo import ZoneInfo
+
+from twitchAPI.chat import Chat, ChatCommand, ChatUser
+
+from fishing.fishing_db_interaction import add_fish, add_shark, get_shark_names, is_net_available  # noqa
+from fishing.utils import Boost, Fish, Nets, Rarity, Shark, SharkRarity, Size
+
 
 # Fishing Functionality
 class Fishing:
@@ -19,38 +22,45 @@ class Fishing:
         return Rarity.COMMON
 
     # helper function
-    async def __get_caught_item(self, username: str, net_used: Nets, boost: Boost, user_id: int = 0) -> Fish | Shark:
+    async def __get_caught_item(self, user: ChatUser, net_used: Nets, boost: Boost) -> Fish | Shark:
         catch_type = randint(1, 100)
         if catch_type <= 5:
             names = await get_shark_names()
             idx = randint(0, len(names) - 1)
             shark = names[idx]
-            return Shark(user_id, net_used, shark, SharkRarity.VERY_COMMON, Rarity.COMMON, boost)
+            time = datetime.now().astimezone(ZoneInfo("America/Chicago"))
+            time_caught = f"{time.date()} {time.hour}"
+            return Shark(user.name, int(user.id), net_used, shark, SharkRarity.VERY_COMMON, Rarity.COMMON, boost, time_caught)
         elif catch_type <= 25:
             fish_rarity = self.__get_fish_rarity()
-            return Fish(fish_rarity, net_used, Size.LARGE, boost)
+            return Fish(user.name, int(user.id), fish_rarity, net_used, Size.LARGE, boost)
         if catch_type <= 50:
             fish_rarity = self.__get_fish_rarity()
-            return Fish(fish_rarity, net_used, Size.MEDIUM, boost)
+            return Fish(user.name, int(user.id), fish_rarity, net_used, Size.MEDIUM, boost)
         if catch_type <= 80:
             fish_rarity = self.__get_fish_rarity()
-            return Fish(fish_rarity, net_used, Size.SMALL, boost)
-        return Fish(Rarity.TRASH, net_used, Size.LARGE, boost)
+            return Fish(user.name, int(user.id), fish_rarity, net_used, Size.SMALL, boost)
+        return Fish(user.name, int(user.id), Rarity.TRASH, net_used, Size.LARGE, boost)
 
     async def fishing_cmd(self, cmd: ChatCommand):
-        available = is_net_available(cmd.name, cmd.text[len('!fishing') :])
-        if not available:
-            net = Nets("rope net")
+        if cmd.text[len("!fishing") :] != "":
+            available = is_net_available(cmd.name, cmd.text[len("!fishing") :])
+            if not available:
+                net = Nets("rope net")
+                await cmd.reply("This net is not available or I could not find it. Using basic net.")
+            else:
+                net = Nets(cmd.text[len("!fishing") :])
         else:
-            net = Nets(cmd.text[len('!fishing') :])
+            net = Nets("rope net")
 
         odd = randint(0, 99)
         catch_odds = net.get_catch_odds
         if odd <= catch_odds:
             boost = Boost(False, 2)
-            catch = await self.__get_caught_item(cmd.name, net, boost)
+            catch = await self.__get_caught_item(cmd.user, net, boost)
             if isinstance(catch, Shark):
-                time = datetime.now().astimezone(ZoneInfo("America/Chicago"))
-                time_caught = f"{time.date()} {time.hour}"
-                await add_shark(cmd.name, SharkRarity.COMMON, time_caught, 0)
+                await add_shark(catch)
+                await cmd.reply(f"You have successfully caught a {catch.name}! You got {catch.coin_value} coins.")
                 return
+            await add_fish(catch)
+            await cmd.reply(f"You have successfully caught a {catch.rarity} fish! You got {catch.coin_value} coins.")
